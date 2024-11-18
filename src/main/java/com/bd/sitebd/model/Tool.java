be37d39284dba16b99c0be0d3c220eace3f6208f
LidiaMessias <lidia.mess@gmail.com>
2024-11-18 10:32:25 -0300

    Primeiro commit

---
+++ src/main/java/com/bd/sitebd/model/Tool.java
@@ -0,0 +1,12 @@
+package com.bd.sitebd.model;
+
+import java.util.Map;
+
+public class Tool {
+    public static Cliente converterCliente(Map<String,Object> registro) {
+        // Como regisro.get retorna Object, devemos usar o polimorfismo de subtipos (downcast) para recuperar os tipos originais
+        return new Cliente((Integer) registro.get("id"),
+                            (String) registro.get("nome"), 
+                            (String) registro.get("cpf"));
+    }
+}
---
+++ src/main/resources/schema-postgresql.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS cliente (
+    /* Serial = inteiro e autoincrement */
+    id serial PRIMARY KEY,
+    nome VARCHAR(50),
+    cpf VARCHAR(11)
+);
+
